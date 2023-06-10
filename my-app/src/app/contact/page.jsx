93aafdbe777bdee28d674e9fd761fd7235a47c0a
juliasydor <juliasydor.git@gmail.com>
2023-06-10 18:56:02 -0300

    update

--- my-app/src/app/contact/page.jsx
+++ my-app/src/app/contact/page.jsx
@@ -7,17 +7,17 @@ const ContactPage = () => {
       <main id='mainContactPage'>
         <div className='containerContactPage'>
           <div className='contentContactPage'> 
-            <h1>Fale conosco</h1>
-            <form action="">
-              <input type="text" name="nome" placeholder='insira seu nome' className='ContactInput'/>
-              <input type="email" name="email" placeholder='insira seu email' className='ContactInput'/>
-              <input type="number" name="number" placeholder='insira seu número' className='ContactInput'/>
-              <textarea name="email" id="contactTextArea" cols="30" rows="10" placeholder='Digite aqui seu projeto ou solicite um orçamento'></textarea>
-              <button>Enviar</button>
-            </form>
+            <h1><span>Fale</span> conosco</h1>
+              <form action="">
+                <input type="text" name="nome" placeholder='insira seu nome' className='ContactInput'/>
+                <input type="email" name="email" placeholder='insira seu email' className='ContactInput'/>
+                <input type="number" name="number" placeholder='insira seu número' className='ContactInput'/>
+                <textarea name="email" id="contactTextArea" cols="30" rows="10" placeholder='Digite aqui seu projeto ou solicite um orçamento'></textarea>
+                <button>Enviar</button>
+              </form>
           </div>
           <div className='ilustrationContactPage'>
-          <img src="/contact.png" alt="ilustração"/>
+          <img src="/contact.png" alt="ilustração" id="imgContactPage" />
           </div>
         </div>
       </main>
